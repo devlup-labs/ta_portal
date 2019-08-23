fc79f893df71f59d9226043c15b54ebf76be1836
Tanuj22 <agarwal.5@iitj.ac.in>
2019-08-24 02:15:43 +0530

    removed comma and space

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -13,7 +13,7 @@ class TeachingAssistantProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     roll_no = models.CharField(max_length=9, blank=True)
     program = models.CharField(max_length=1, choices=PROGRAMS)
-    phone = models.CharField(max_length=10,)
+    phone = models.CharField(max_length=10)
     alternate_phone = models.CharField(max_length=10, blank=True)
     research_area = models.CharField(max_length=100, blank=True)
     educational_background = models.CharField(max_length=20)
