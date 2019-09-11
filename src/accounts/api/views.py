8c2be81ba12b66f65b36f5341ad3c244efee1ef3
Tanuj22 <agarwal.5@iitj.ac.in>
2019-09-11 23:30:00 +0530

    created api for userprofile and login

---
+++ src/accounts/api/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets
+from accounts.api.serializers import TeachingAssistantProfileSerializer, UserSerializer
+from accounts.models import TeachingAssistantProfile
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TeachingAssistantViewSet(viewsets.ModelViewSet):
+    serializer_class = TeachingAssistantProfileSerializer
+    queryset = TeachingAssistantProfile.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class AuthenticationCheckAPIView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, *args, **kwargs):
+        authenticated = request.user.is_authenticated
+        data = {
+            'message': 'Authorized' if authenticated else 'Unauthorized'
+        }
+        status_code = status.HTTP_200_OK if authenticated else status.HTTP_401_UNAUTHORIZED
+        return Response(data, status=status_code)
