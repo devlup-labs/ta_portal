f8a8aaba24b914d2a1b52adcbb2aebefeb751e0e
Tanuj22 <agarwal.5@iitj.ac.in>
2019-09-17 16:54:39 +0530

    set up axios

---
+++ frontend/src/plugins/httpClient.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { BACKEND_API_ADDRESS } from "../config";
+import store from "../store";
+import router from "../router";
+
+const httpClient = axios.create({
+  //baseURL: BACKEND_API_ADDRESS,
+  xsrfCookieName: "csrftoken",
+  xsrfHeaderName: "X-CSRFToken"
+});
+
+httpClient.interceptors.response.use(
+  response => response,
+  error => {
+    if (
+      error.response.status === 401 &&
+      router.currentRoute.name !== "Login" &&
+      router.currentRoute.meta.requiresAuth
+    ) {
+      store
+        .dispatch("auth/logout")
+        .then(() => {
+          router.push({ name: "Login" });
+        })
+        .catch(() => {});
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export { httpClient };
+
+export default {
+  install(Vue, options) {
+    Vue.prototype.$httpClient = httpClient;
+  }
+};
