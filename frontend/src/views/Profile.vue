12ffee4c68b84dcfea28dcca3330b3d4baf775c1
Jashpatel1 <patel.5@iitj.ac.in>
2019-09-17 12:16:12 +0530

    fixed app

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,16 +1,13 @@
 <template lang="pug">
     v-row(justify="center")
-        HeaderToolbar
         ProfileCard
 </template>
 
 <script>
-import HeaderToolbar from "../components/HeaderToolbar.vue";
 import ProfileCard from "../components/ProfileCard.vue";
 export default {
   name: "profile",
   components: {
-    HeaderToolbar,
     ProfileCard
   }
 };
