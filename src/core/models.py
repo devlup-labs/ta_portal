68527d9c4437320c6d688bc5865681ffda22c433
Tanuj22 <agarwal.5@iitj.ac.in>
2019-09-19 01:35:18 +0530

    changed attribuites in model

--- src/core/models.py
+++ src/core/models.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class Course(models.Model):
     name = models.CharField(max_length=128)
-    course_code = models.CharField(max_length=8)
+    code = models.CharField(max_length=8, help_text="Enter the Course Code")
     supervisor = models.ForeignKey(TeachingAssistantSupervisorProfile, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -31,9 +31,9 @@ class Feedback(models.Model):
 
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
     duties_completed = models.CharField(max_length=256)
-    supervisors_comments = models.CharField(max_length=256, blank=True, null=True)
+    comment = models.CharField(max_length=256, blank=True, null=True, help_text="Enter Supervisor Comment")
     status = models.CharField(max_length=1, choices=STATUS, default='1')
-    date_submitted = models.DateTimeField(auto_now_add=True, null=True)
+    date_submitted = models.DateTimeField(auto_now_add=True)
     date_approved = models.DateTimeField(blank=True, null=True)
 
     def save(self, force_insert=False, force_update=False, using=None,
