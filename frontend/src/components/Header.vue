d78837547c8e477db433c9e67e6866b4fecaea96
Jashpatel1 <43696490+Jashpatel1@users.noreply.github.com>
2019-09-18 17:56:49 +0530

    headertoolbar component added (#15)
    * headertoolbar specifically added
    
    * fixed logincard
    
    * fixed app
    
    * v-if used
    
    * dropdown added
    
    * prettier

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    v-app-bar(app clipped-left color="primary")
+        v-app-bar-nav-icon.white--text(@click="$emit('toggleDrawer')")
+        v-toolbar-title.white--text
+            span.title.ml-3.mr-5 TA Portal&nbsp;
+            span.font-weight-light Department of Electrical Engineering, IIT Jodhpur
+        v-spacer
+        v-menu(v-if="profile" bottom left)
+            template(v-slot:activator="{ on }")
+                v-btn(dark icon v-on="on")
+                    v-avatar(size="38px")
+                        img(src="https://cdn.vuetifyjs.com/images/logos/logo.svg")
+            v-list
+                v-list-item(
+                    v-for="(item, i) in menu"
+                    :key="i"
+                    @click="")
+                    v-list-item-title {{ item.title }}
+        v-btn.white--text(v-else outlined :to="{name: 'login'}") Login
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["profile"],
+  data() {
+    return {
+      menu: [{ title: "View Profile" }, { title: "Logout" }]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
--- frontend/src/components/ProfileCard.vue
+++ frontend/src/components/ProfileCard.vue
@@ -1,36 +1,34 @@
 <template lang="pug">
-    v-card(flat)
-        v-card-title(justify="center")
-            h2(class="headline") User Profile
-        v-card-text
-            v-container
-                v-row
-                    v-col(cols="12")
-                        v-text-field(solo-inverted label="Name" required)
-                    v-col(cols="12" sm="6" md="6")
-                        v-text-field(solo-inverted label="Roll no.")
-                    v-col(cols="12" sm="6" md="6")
-                        v-text-field(solo-inverted label="Programme" required)
-                    v-col(cols="12")
-                        v-text-field(solo-inverted label="Email" required)
-                    v-col(cols="12")
-                        v-text-field(solo-inverted label="Contact no." required)
-                        h3 Educational Background
-                v-row
-                    v-col(cols="12" sm="6")
-                        v-select(solo-inverted :items="['Electrical Engineering', 'Electronics and Comunication Engineering', ]"
-                            label="B.Tech Specialisation"
-                            required)
-                    v-col(cols="12" sm="6")
-                        v-select(solo-inverted :items="['Electrical Engineering', 'Electronics and Comunication Engineering', 'Any other option' ]"
-                            label="M.Sc/M.Tech Specialisation"
-                            multiple)
-                    v-col(cols="12")
-                        v-text-field(solo-inverted label="Area of Research" required)
-        v-card-actions
-            div(class="flex-grow-1")
-            v-btn(color="blue darken-1" text @click="dialog = false") Close
-            v-btn(color="blue darken-1" text @click="dialog = false") Save
+        v-card(flat)
+            v-card-title(justify="center")
+                h2(class="headline") User Profile
+            v-card-text
+                v-container
+                    v-row
+                        v-col(cols="12")
+                            v-text-field(solo-inverted label="Name" required)
+                        v-col(cols="12" sm="6" md="6")
+                            v-text-field(solo-inverted label="Roll no.")
+                        v-col(cols="12" sm="6" md="6")
+                            v-text-field(solo-inverted label="Programme" required)
+                        v-col(cols="12")
+                            v-text-field(solo-inverted label="Email" required)
+                        v-col(cols="12")
+                            v-text-field(solo-inverted label="Contact no." required)
+                            h3 Educational Background
+                    v-row
+                        v-col(cols="12" sm="6")
+                            v-select(solo-inverted :items="['Electrical Engineering', 'Electronics and Comunication Engineering', ]"
+                                label="B.Tech Specialisation"
+                                required)
+                        v-col(cols="12" sm="6")
+                            v-text-field(solo-inverted label="M.Tech/M.Sc Specialisation")
+                        v-col(cols="12")
+                            v-text-field(solo-inverted label="Area of Research" required)
+            v-card-actions
+                div(class="flex-grow-1")
+                v-btn(color="blue darken-1" text @click="dialog = false") Close
+                v-btn(color="blue darken-1" text @click="dialog = false") Save
 </template>
 
 <style scoped></style>
