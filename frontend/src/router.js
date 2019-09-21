ad689aa4f00b4bce7385f51845454d7306525617
arpitgupta1906 <gupta.25.iitj.ac.in>
2019-09-21 16:31:35 +0530

    removed ftest

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 import Login from "./views/Login";
 import Profile from "./views/Profile";
-import FeedbackTextBox from "./components/FeedbackTextBox";
 Vue.use(Router);
 
 export default new Router({
@@ -18,11 +17,6 @@ export default new Router({
       path: "/profile",
       name: "profile",
       component: Profile
-    },
-    {
-      path:"/FeedbackTextBox",
-      name:"FeedbacFeedbackTextBox",
-      component:FeedbackTextBox
     },
     {
       path: "/about",
