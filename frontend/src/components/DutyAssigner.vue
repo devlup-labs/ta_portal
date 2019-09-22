50e29567a38dec96dc7fb4f3fd2da6d2b044a218
ajatprabha <ajat.prabha.leo@gmail.com>
2019-09-22 16:19:03 +0530

    [WIP] Add DutyAssigner component

---
+++ frontend/src/components/DutyAssigner.vue
@@ -0,0 +1,138 @@
+<template lang="pug">
+  v-data-table(
+    v-model="selected"
+    :headers="headers"
+    :items="desserts"
+    :single-select="singleSelect"
+    item-key="name"
+    show-select
+    class="elevation-1")
+    template(v-slot:top)
+      v-toolbar(color="accent" :prominent="false")
+        v-toolbar-title Assigner
+        template(v-slot:extension)
+          v-row
+            v-col(cols="6")
+              v-tabs(v-model="model" background-color="transparent")
+                v-tab(v-for="(item, i) in programs" :key="i")
+                  | {{ item }}
+            v-col(cols="6" align-self="center")
+              v-row.px-3
+                span
+                  strong SELECTED:&nbsp;
+                  | 4 TAs
+                v-spacer
+                v-btn(color="primary" small rounded) Save
+      v-row
+        v-col
+          v-text-field.mx-4(append-icon="mdi-mic" flat hide-details label="Search"
+            prepend-icon="mdi-magnify" outlined)
+</template>
+
+<script>
+export default {
+  name: "DutyAssigner",
+  data: () => ({
+    singleSelect: false,
+    selected: [],
+    programs: ["PhD", "MTech"],
+    headers: [
+      {
+        text: "Dessert (100g serving)",
+        align: "left",
+        sortable: false,
+        value: "name"
+      },
+      { text: "Calories", value: "calories" },
+      { text: "Fat (g)", value: "fat" },
+      { text: "Carbs (g)", value: "carbs" },
+      { text: "Protein (g)", value: "protein" },
+      { text: "Iron (%)", value: "iron" }
+    ],
+    desserts: [
+      {
+        name: "Frozen Yogurt",
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: "1%"
+      },
+      {
+        name: "Ice cream sandwich",
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: "1%"
+      },
+      {
+        name: "Eclair",
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: "7%"
+      },
+      {
+        name: "Cupcake",
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        iron: "8%"
+      },
+      {
+        name: "Gingerbread",
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: "16%"
+      },
+      {
+        name: "Jelly bean",
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: "0%"
+      },
+      {
+        name: "Lollipop",
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: "2%"
+      },
+      {
+        name: "Honeycomb",
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: "45%"
+      },
+      {
+        name: "Donut",
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: "22%"
+      },
+      {
+        name: "KitKat",
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: "6%"
+      }
+    ]
+  })
+};
+</script>
+
+<style scoped></style>
