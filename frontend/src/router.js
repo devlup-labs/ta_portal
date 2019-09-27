ab8dad1bfeb1d3487fe8c162d890d174a55832f2
arpitgupta1906 <gupta.25.iitj.ac.in>
2019-09-27 21:28:53 +0530

    resolved conflicts

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 import Login from "./views/Login";
 import Profile from "./views/Profile";
-import FeedbackTextBox from "./components/FeedbackTextBox";
 Vue.use(Router);
 
 export default new Router({
@@ -13,11 +12,6 @@ export default new Router({
       path: "/login",
       name: "login",
       component: Login
-    },
-    {
-      path: "/ftest",
-      name: "ftest",
-      component: FeedbackTextBox
     },
     {
       path: "/profile",
