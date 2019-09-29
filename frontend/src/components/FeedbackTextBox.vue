d7922783af32f7ab36a8529a2667052557b89e35
arpitgupta1906 <43808283+arpitgupta1906@users.noreply.github.com>
2019-09-29 19:54:57 +0530

    added feedback textbox component (#20)
    * added feedback textbox component
    
    * defined css of items in tags
    
    * removed ftest
    
    * used less css
    
    * removed css
    
    * resolved conflicts
    
    * removed spaces
    
    * added a tab
    
    * implemended classes in a better way

---
+++ frontend/src/components/FeedbackTextBox.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+
+    v-card.rounded-card
+        v-container.pa-2
+            v-card-title
+                 v-row
+                    h4 EE213,Signals and Systems
+                    v-col.text-right
+                        v-chip.ma-2(color="#e3cceb" align="right" )  Submitted on 20th September,2019
+            h3 Completed TA Duties
+
+                div(class="text-center")
+                    v-card-text
+                        v-row
+                            v-textarea(
+
+                                    name="input-7-1"
+                                    filled
+                                    background-color="white"
+                                    label=""
+                                    auto-grow
+                                    rounded
+                                    value="Tutorial and attendance of EE batch(Tuesday and Thursday)"
+                                    outlined
+                            )
+                        v-row
+                            v-textarea(
+
+                                name="input-7-1"
+                                filled
+                                background-color="white"
+                                label=""
+                                auto-grow
+                                rounded
+                                value="Tutorial and attendance of EE batch(Tuesday and Thursday)"
+                                outlined
+                            )
+                div(align="right")
+                    v-btn.ma-2(width=200 rounded outlined color="blue" right) Close
+</template>
+
+<script>
+export default {
+    name: "FeedbackTextBox",
+};
+</script>
+
+<style scoped>
+
+
+
+</style>
